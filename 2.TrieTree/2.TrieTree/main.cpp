297f8a8496643959d66ba6b2f9c9ec994ed98206
Yao <306097648@qq.com>
2016-08-01 16:26:36 +0800

    2.TrieTree AC

--- 2.TrieTree/2.TrieTree/main.cpp
+++ 2.TrieTree/2.TrieTree/main.cpp
@@ -12,26 +12,40 @@
 using namespace std;
 
 class TrieTreeNode{
-	int wordCount;
-	bool isWord;
 public:
+	int wordCount;
+	bool wordEnd;
 	TrieTreeNode *next[26];
-	TrieTreeNode(int wc, bool iw):wordCount(wc), isWord(iw){
+	
+	TrieTreeNode():wordCount(0), wordEnd(0){
 		for (int i = 0; i < 26; ++i)
 			next[i] = NULL;
 	}
-	~TrieTreeNode(){
-		
-	}
 };
 
-void initTree(TrieTreeNode* head, string& s){
-	
+void initTree(TrieTreeNode* head, string& s, int deep){
+	head->wordCount++;
+	if (deep == s.size()){
+		head->wordEnd = 1;
+		return ;
+	}
+	int c = s[deep] - 'a';
+	if (head->next[c] == NULL){
+		head->next[c] = new TrieTreeNode();
+	}
+	initTree(head->next[c], s, deep + 1);
 }
 
-int search(TrieTreeNode* head, string &s){
-	return 0;
+int search(TrieTreeNode* head, string &s, int deep){
+	if (deep == s.size())
+		return head->wordCount;
+	int c = s[deep] - 'a';
+	if (head->next[c] == NULL)
+		return 0;
+	else
+		return search(head->next[c], s, deep + 1);
 }
+
 void delTree(TrieTreeNode* head){
 	for (int i = 0; i < 26; i++){
 		if (head->next[i] != NULL)
@@ -44,14 +58,15 @@ int main(int argc, const char * argv[]) {
 	int n, m;
 	string s;
 	cin >> n;
-	TrieTreeNode *head = new TrieTreeNode(0, 0);
+	TrieTreeNode *head = new TrieTreeNode();
 	while(n--){
 		cin >> s;
-		initTree(head, s);
+		initTree(head, s, 0);
 	}
 	cin >> m;
 	while(m--){
-		int wordCount = search(head, s);
+		cin >> s;
+		int wordCount = search(head, s, 0);
 		cout << wordCount << endl;
 	}
 	delTree(head);
