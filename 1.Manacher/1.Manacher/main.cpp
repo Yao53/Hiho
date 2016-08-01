5dc58928435fc42048e28b4ae320f39d0ff18905
Yao <306097648@qq.com>
2016-08-01 15:29:00 +0800

    Manacher Finished

---
+++ 1.Manacher/1.Manacher/main.cpp
@@ -0,0 +1,50 @@
+//
+//  main.cpp
+//  1.Manacher
+//
+//  Created by Yao on 16/8/1.
+//  Copyright © 2016年 San. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(){
+	int N;
+	string inputS;
+	string s;
+	cin >> N;
+	while(N--){
+		cin >> inputS;
+		s = "$#";
+		for(int i = 0; i < inputS.size(); ++i){
+			s += inputS[i];
+			s += "#";
+		}
+		s += "%";
+		cout<< s<<endl;
+		int maxLen = 0;
+		int maxRegion = 0;
+		int maxCenter = 1;
+		vector<int> region(s.size());
+		for (int i = 2; i <= s.size() - 2; ++i){
+			if (maxCenter + region[maxCenter] > i){
+				region[i] = min(maxCenter + region[maxCenter] - i, region[2 * maxCenter - i]);
+			}
+			else {
+				region[i] = 1;
+			}
+			while(s[i - region[i]] == s[i + region[i]])
+				++region[i];
+			if (i + region[i] > maxCenter + region[maxCenter]){
+				maxCenter = i;
+			}
+			if (region[maxCenter] > maxLen)
+				maxLen = region[maxCenter];
+		}
+		cout << maxLen - 1 << endl;
+	}
+	return 0;
+}
