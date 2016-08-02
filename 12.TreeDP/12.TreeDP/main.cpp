3865a68ab1770fcdd6f9542fe97ae46b7c015189
Yao <306097648@qq.com>
2016-08-02 15:14:09 +0800

    12.TreeDP init

---
+++ 12.TreeDP/12.TreeDP/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  12.TreeDP
+//
+//  Created by Yao on 16/8/2.
+//  Copyright © 2016年 San. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Node{
+	int cost;
+	
+};
+
+int main(int argc, const char * argv[]) {
+	
+	int N, M;
+	cin >> N >> M;
+	int *c = (int *)malloc(N * sizeof(int));
+	
+	free(c);
+    return 0;
+}
