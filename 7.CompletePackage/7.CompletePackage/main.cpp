18a7f6e6e5d819a279c017209c9aff05098e087a
Yao <306097648@qq.com>
2016-08-04 17:32:05 +0800

    7.CompletePackage AC

--- 7.CompletePackage/7.CompletePackage/main.cpp
+++ 7.CompletePackage/7.CompletePackage/main.cpp
@@ -7,9 +7,28 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
+
+void CompletePackage(int *dp, int c, int w, int M){
+	for (int v = c; v <= M; ++v)
+		dp[v] = max(dp[v], dp[v - c] + w);
+}
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	int n, m;
+	cin >> n >> m;
+	int *c = (int*)malloc(n * sizeof(int));
+	int *w = (int*)malloc(n * sizeof(int));
+	int *dp = (int*)malloc(m * sizeof(int));
+	memset(dp, 0, m * sizeof(int));
+	for (int i = 0; i < n; ++i)
+		cin >> c[i] >> w[i];
+	for (int i = 0; i < n; ++i)
+		CompletePackage(dp, c[i], w[i], m);
+	cout << dp[m] << endl;
+	free(c);
+	free(w);
+	free(dp);
     return 0;
 }
